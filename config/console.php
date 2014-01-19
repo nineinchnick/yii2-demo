19760ab8468b798a20afc67dbbabc1894e4ece89
John Was <janek.jan@gmail.com>
2014-01-20 00:31:18 +0100

    added migrations for notifications module and auth schema

--- config/console.php
+++ config/console.php
@@ -7,7 +7,20 @@ return [
 	'controllerPath' => dirname(__DIR__) . '/commands',
 	'controllerNamespace' => 'app\commands',
 	'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
+	'aliases' => [
+		'@nineinchnick/usr' => '@vendor/nineinchnick/yii2-usr',
+		'@nineinchnick/nfy' => '@vendor/nineinchnick/yii2-nfy',
+	],
+	'modules' => [
+		'nfy' => [
+			'class' => 'nineinchnick\nfy\Module',
+			'queues' => ['mq'],
+		],
+	],
 	'components' => [
+		'authManager' => [
+			'class' => 'yii\rbac\DbManager',
+		],
 		'cache' => [
 			'class' => 'yii\caching\FileCache',
 		],
