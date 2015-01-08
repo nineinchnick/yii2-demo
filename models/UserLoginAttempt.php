2f283194869125851cba29acc64c2f7b1fcb9d36
Jan Waś <janek.jan@gmail.com>
2015-01-08 01:49:56 +0100

    more yii2-usr models

---
+++ models/UserLoginAttempt.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+/**
+ * @inheritdoc
+ */
+class UserLoginAttempt extends \nineinchnick\usr\models\ExampleUserLoginAttempt
+{
+}
---
+++ models/UserRemoteIdentity.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+/**
+ * @inheritdoc
+ */
+class UserRemoteIdentity extends \nineinchnick\usr\models\ExampleUserRemoteIdentity
+{
+}
